ce27edcae79b3ab5a7619db56d663caa686a2fed
Adamantios <manos_0_7@hotmail.com>
2016-10-15 17:17:15 +0300

    Clean code a bit.

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -1,9 +1,8 @@
 (function () {
 
-  angular.module('app.controllers', ['app.controllers.statistics', 'app.controllers.signup',
-    'app.controllers.profile', 'app.controllers.offers', 'app.controllers.login', 'app.controllers.home',
-    'app.controllers.first.address', 'app.controllers.farmit', 'app.controllers.evaluation', 'app.controllers.chat',
-    'app.controllers.announcement', 'app.controllers.address', 'app.controllers.provider', 'app.controllers.delete.profile',
-    'app.controllers.logout', 'app.controllers.cart'])
+  angular.module('app.controllers', ['app.controllers.statistics', 'app.controllers.profile',
+    'app.controllers.offers', 'app.controllers.home', 'app.controllers.first.address', 'app.controllers.farmit',
+    'app.controllers.evaluation', 'app.controllers.chat', 'app.controllers.announcement', 'app.controllers.address',
+    'app.controllers.provider', 'app.controllers.cart'])
 
 })();
--- www/js/controllers/profile.js
+++ www/js/controllers/profile.js
@@ -8,4 +8,135 @@
     .controller('ProfileCtrl', function ($scope) {
 
     })
+
+    .controller('SignUpCtrl', function ($scope, $window, $ionicPopup, $state, SignUpService) {
+
+      $scope.isSpinning = false;
+      $scope.login = {'isChecked': false};
+
+      $scope.farmIn = function ($name, $surname, $email, $password, $tel) {
+        $scope.isSpinning = true;
+
+        SignUpService.signUp($name, $surname, $email, $password, $tel).then(function ($success) {
+          $window.localStorage.setItem('remember_me', $scope.login['isChecked']);
+          $window.localStorage.setItem('token', $success.data.token);
+          $window.localStorage.setItem('email', $success.data.session.email);
+          $scope.isSpinning = false;
+          $state.go('first-address');
+        }, function (error) {
+          $scope.isSpinning = false;
+
+          var $message;
+
+          if (error.status == 409)
+            $message = 'An account with that email already exists!';
+          else
+            $message = 'Something went wrong while trying to signup! Please try again!';
+
+          // Alert dialog
+          $ionicPopup.alert({
+            title: 'Error!',
+            template: $message
+          });
+        });
+      };
+    })
+
+    .controller('LogInCtrl', function ($scope, $window, $ionicPopup, $state, LogInService) {
+      $scope.isSpinning = false;
+      $scope.login = {'isChecked': false};
+
+      $scope.farmIn = function ($email, $password) {
+        $scope.isSpinning = true;
+
+        LogInService.logIn($email, $password).then(function ($success) {
+          $window.localStorage.setItem('remember_me', $scope.login['isChecked']);
+          $window.localStorage.setItem('token', $success.data.token);
+          $window.localStorage.setItem('email', $success.data.session.email);
+          $scope.isSpinning = false;
+          $state.go('home.menu-content');
+        }, function () {
+          $scope.isSpinning = false;
+
+          var $message;
+
+          if ($error.status == 403)
+            $message = 'There is no account with that email!';
+          else
+            $message = 'Something went wrong while trying to login! Please try again!';
+
+          // Alert dialog
+          $ionicPopup.alert({
+            title: 'Error!',
+            template: $message
+          });
+        });
+      }
+    })
+
+    .controller('LogOutCtrl', function ($scope, $ionicPopup, $state, $window, $ionicSideMenuDelegate, LogOutService) {
+      $scope.logOut = function () {
+        // Confirm dialog
+        var confirmPopup = $ionicPopup.confirm({
+          title: 'Log Out',
+          template: 'Are you sure you want to log out?'
+        });
+
+        confirmPopup.then(function ($ok) {
+          if ($ok) {
+            LogOutService.logOut($window.localStorage.getItem('email')).then(function () {
+              $ionicSideMenuDelegate.toggleLeft();
+              $window.localStorage.setItem('remember_me', false);
+              $window.localStorage.setItem('token', null);
+              $window.localStorage.setItem('email', null);
+              $scope.isSpinning = false;
+              $state.go('farmit');
+            }, function () {
+              // Alert dialog
+              $ionicPopup.alert({
+                title: 'Error!',
+                template: 'Something went wrong while trying to log out! Please try again!'
+              });
+            });
+          }
+        });
+      };
+    })
+
+    .controller('DeleteProfileCtrl', function ($scope, $state, $ionicPopup, $window, $ionicSideMenuDelegate,
+                                               DeleteProfileService) {
+
+      $scope.isSpinning = false;
+
+      $scope.goodbye = function ($email, $password) {
+        $scope.isSpinning = true;
+
+        if ($email == $window.localStorage.getItem('email')) {
+          DeleteProfileService.deleteProfile($email, $password).then(function () {
+              $ionicSideMenuDelegate.toggleLeft();
+              $window.localStorage.setItem('remember_me', false);
+              $window.localStorage.setItem('token', null);
+              $window.localStorage.setItem('email', null);
+              $scope.isSpinning = false;
+              $state.go('farmit');
+            },
+            function () {
+              $scope.isSpinning = false;
+              // Alert dialog
+              $ionicPopup.alert({
+                title: 'Error!',
+                template: 'Something went wrong while trying to delete your profile! Please try again!'
+              });
+            });
+        }
+
+        else {
+          // Alert dialog
+          $ionicPopup.alert({
+            title: 'Wrong email!',
+            template: 'This is not the email you are signed in with!'
+          });
+        }
+      }
+    })
 })();
