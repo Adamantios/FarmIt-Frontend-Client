3448983891c9e7c30e4e4a2a69c37fb209aaf1ee
Adamantios <manos_0_7@hotmail.com>
2016-10-25 21:03:31 +0300

    Complete offers functionality.

--- www/js/controllers/offers.js
+++ www/js/controllers/offers.js
@@ -1,33 +1,68 @@
 (function () {
 
   angular.module('app.controllers.offers', [])
-    .controller('OffersCtrl', function ($scope, $ionicPopup, OffersService) {
+    .controller('OffersCtrl', function ($scope, $state, $ionicPopup, $interval, OffersService) {
       $scope.userOffers = {};
       $scope.userOffers.offers = [];
-      $scope.userOffers.isSpinning = true;
+      $scope.userOffers.isSpinning = false;
 
-      OffersService.getOffers().then(function ($success) {
-          $scope.userOffers.isSpinning = false;
-          $scope.userOffers.offers = $success.data.data;
-        },
-        function () {
-          $scope.userOffers.isSpinning = false;
-        });
+      $scope.loadOffers = function () {
+        $scope.userOffers.isSpinning = true;
+        OffersService.getOffers().then(function ($success) {
+            $scope.userOffers.isSpinning = false;
+            $scope.userOffers.offers = $success.data.data;
+          },
+          function () {
+            $scope.userOffers.isSpinning = false;
+          });
+      };
 
-      $scope.chooseOffer = function () {
-        $ionicPopup.show({
+      $scope.chooseOffer = function ($id) {
+        var $popup = $ionicPopup.show({
           title: 'Accept Offer',
           template: 'Would you like to accept this offer?',
-          buttons:[
+          buttons: [
             {
-              text:'Yes',
-              type:'button-positive',
+              text: 'Yes',
+              type: 'button-positive',
               onTap: function () {
-
+                $scope.acceptOffer($popup, $id);
               }
             }
           ]
         })
-      }
+      };
+
+      $scope.acceptOffer = function ($popup, $id) {
+        $popup.close();
+
+        OffersService.acceptOffer($id).then(function () {
+            $scope.loadOffers();
+            $ionicPopup.alert({
+              title: 'Excellent choice!',
+              template: 'An email with your purchase details have been sent to your account.' +
+              ' In the mean time... Farmit some more!'
+            });
+          },
+          function () {
+            $ionicPopup.alert({
+              title: 'Offer not accepted!',
+              template: 'Something went wrong while trying to accept this offer. Please try again later.'
+            });
+          });
+      };
+
+      $scope.doRefresh = function() {
+        $scope.loadOffers();
+        // Stop the ion-refresher from spinning
+        $scope.$broadcast('scroll.refreshComplete');
+      };
+
+      $scope.loadOffers();
+
+      // Refresh offers every minute.
+      $interval(function () {
+        $scope.loadOffers();
+      }, 60 * 1000);
     })
 })();
--- www/js/services/offers.js
+++ www/js/services/offers.js
@@ -20,6 +20,25 @@
             .error(function ($returnedData) {
               return $returnedData;
             });
+        },
+
+        acceptOffer: function ($offer_id) {
+          var $url = $rootScope.server + 'api/offers/accept_offer';
+
+          var $parameters =
+          {
+            "email": $window.localStorage.getItem('email'),
+            "token": $window.localStorage.getItem('token'),
+            "offer_id": $offer_id
+          };
+
+          return $http.post($url, $parameters)
+            .success(function ($returnedData) {
+              return $returnedData;
+            })
+            .error(function ($returnedData) {
+              return $returnedData;
+            });
         }
       };
     })
