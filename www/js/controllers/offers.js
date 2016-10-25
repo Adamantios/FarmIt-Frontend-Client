57f43356c36882ed4a488f41af7f32157fa1a9dd
Adamantios <manos_0_7@hotmail.com>
2016-10-25 20:25:06 +0300

    Add more functionality at offers.

--- www/js/controllers/offers.js
+++ www/js/controllers/offers.js
@@ -1,16 +1,33 @@
 (function () {
 
   angular.module('app.controllers.offers', [])
-    .controller('OffersCtrl', function ($scope, OffersService) {
+    .controller('OffersCtrl', function ($scope, $ionicPopup, OffersService) {
       $scope.userOffers = {};
       $scope.userOffers.offers = [];
-      $scope.userOffers.isSpinning = false;
+      $scope.userOffers.isSpinning = true;
 
       OffersService.getOffers().then(function ($success) {
-        $scope.userOffers.offers = $success.data.data;
-      },
-      function ($error) {
+          $scope.userOffers.isSpinning = false;
+          $scope.userOffers.offers = $success.data.data;
+        },
+        function () {
+          $scope.userOffers.isSpinning = false;
+        });
 
-      });
+      $scope.chooseOffer = function () {
+        $ionicPopup.show({
+          title: 'Accept Offer',
+          template: 'Would you like to accept this offer?',
+          buttons:[
+            {
+              text:'Yes',
+              type:'button-positive',
+              onTap: function () {
+
+              }
+            }
+          ]
+        })
+      }
     })
 })();
